2dea9256fa9af6f8687cf5c73543c449eaed3396
raechel <odom.raechel@gmail.com>
2019-05-23 07:53:09 -0600

    applied grid payout to about section

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 export default function Nav() {
   return (
     <nav className="Nav">
-      <NavLink exact to='/skills' className="nav" >skills</NavLink>
-      <NavLink exact to='/projects' className="nav" >projects</NavLink>
-      <NavLink exact to='/contact' className="nav" >contact</NavLink>
-      <NavLink exact to='/about' className="nav" >raechel<span>odom</span></NavLink>
+      <NavLink exact to='/skills' className="nav-link" >skills</NavLink>
+      <NavLink exact to='/projects' className="nav-link" >projects</NavLink>
+      <NavLink exact to='/contact' className="nav-link" >contact</NavLink>
+      <NavLink exact to='/about' className="nav-link" >raechel<span>odom</span></NavLink>
     </nav>
   )
 }

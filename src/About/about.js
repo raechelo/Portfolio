1dbe970d98e9b004193f27dd9ff96fb6e6005a5b
raechel <odom.raechel@gmail.com>
2019-05-29 10:07:54 -0600

    About section updated

--- src/About/about.js
+++ src/About/about.js
@@ -6,7 +6,7 @@ export default function about() {
       <h2>hi!</h2>
       <h1>i'm raechel</h1>
       <p className="p-info">I'm a front end engineer who loves helping people and developing creative, accesible apps with clean code. Some of my favorite languages and frameworks to use are React, ES6, Scss, and Jest.</p>
-      <p className="p-info2">Outside of the coding world, I love to spend time with my dog, Zeus, to read, I enjoy great sour beer, and I love watching hockey games. Let's go Blues!</p>
+      <p className="p-info2">Outside of the coding world, I love to spend time with my dog, Zeus, I enjoy reading, a great sour beer, and I love watching hockey games. Let's go Blues!</p>
     </section>
   )
 }

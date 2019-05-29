2888619198d0fcc4a47aba92b168164e40c9da75
raechel <odom.raechel@gmail.com>
2019-05-29 10:06:51 -0600

    Skills added and gridded out

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
 import chai from '../assets/chai.png';
 import enzyme from '../assets/enzyme.png';
 import html from '../assets/html.png';
 import jest from '../assets/jest.png';
+import js from '../assets/js.svg';
+import mocha from '../assets/mocha.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import scss from '../assets/scss.png';
 
 const Skills = () => {
   
-  let logos = [chai, enzyme, html, jest]
+  let logos = [chai, enzyme, html, jest, js, mocha, react, redux, scss]
   return (
-    <section>
-    {/* <Route exact to="/skills" component={Skills} /> */}
+    <section className="Skills">
       {logos.map(l => (<img className="logo" src={l} />))}
     </section>
   )

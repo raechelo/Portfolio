592b197fc545879d9976df1cc574e1d56d0a9cc8
raechel <odom.raechel@gmail.com>
2019-06-14 16:40:39 -0600

    resume now appears when users click button

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import resume from '../assets/resume.pdf'
 
 export default function Nav() {
   return (
@@ -9,7 +10,9 @@ export default function Nav() {
         <NavLink exact to='/contact' className="nav-link" >contact</NavLink>
         <NavLink exact to='/skills' className="nav-link" >skills</NavLink>
         <NavLink exact to='/projects' className="nav-link" >projects</NavLink>
+        <form method="get" action={resume}>
         <button><i class="fas fa-download"></i>Download My Resume!</button>
+        </form>
       </nav>
     </section>
 

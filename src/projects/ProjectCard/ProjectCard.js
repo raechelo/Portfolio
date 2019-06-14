935fe76c6b673f4c1e1d3656ac20917671e82701
raechel <odom.raechel@gmail.com>
2019-06-14 16:28:01 -0600

    project data add and appearing in card format

--- src/projects/ProjectCard/ProjectCard.js
+++ src/projects/ProjectCard/ProjectCard.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ProjectCard = props => {
   const { name, link, imgLink, summary, stack } = props
   return (
-    <article>
+    <article className='ProjectCard'>
       <h4>{name}</h4>
-      <h5>{stack}</h5>
-      <a href={link}>
-        <img src={imgLink} />
-      </a>
+      {stack.map(s => <h5 className='stack'>{s}</h5>)}
+      <img src={imgLink} className="project-image" />
+      <a target='blank' href={link}>
+      View Repo</a>
       <p>{summary}</p>
     </article>
   )

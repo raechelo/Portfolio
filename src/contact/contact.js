64b5eb69912063965b8fb7da2b901d2edc480666
raechel <odom.raechel@gmail.com>
2019-07-05 20:10:46 -0600

    btn now works with download resume on click

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import projectData from '../data';
+import resume from '../assets/resume.pdf';
 
 export default function Contact() {
-  const displayData = projectData.contactData.map(c => (<article><img src={c.img} /><a href={c.link}>{c.name}</a></article>))
+  const displayData = projectData.contactData.map(c => (<article><a href={c.link}><img src={c.img} /></a></article>))
+
   return (
     <section className='Contact'>
       <h2>let's connect!</h2>
       <section className="contact-me-section">
         {displayData}
       </section>
+      <form className="download-form" method="get" action={resume}>
+        <button className="download-btn" type="submit">Download my resume!</button>
+      </form>
     </section>
   )
 }

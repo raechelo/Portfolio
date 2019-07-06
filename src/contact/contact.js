2e48fab9eeccea1eaada6d687a0dde74506eb55c
raechel <odom.raechel@gmail.com>
2019-07-05 20:49:29 -0600

    skills mapped out on contact page ;

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import projectData from '../data';
 import resume from '../assets/resume.pdf';
+import chai from '../assets/logos/chai.png';
+import enzyme from '../assets/logos/enzyme.png';
+import html from '../assets/logos/html.png';
+import jest from '../assets/logos/jest.png';
+import js from '../assets/logos/js.svg';
+import mocha from '../assets/logos/mocha.png';
+import react from '../assets/logos/react.png';
+import nodejs from '../assets/logos/nodejs.png';
+import redux from '../assets/logos/redux.png';
+import scss from '../assets/logos/scss.png';
+import css from '../assets/logos/css.png';
+
 
 export default function Contact() {
   const displayData = projectData.contactData.map(c => (<article><a href={c.link}><img src={c.img} /></a></article>))
+
+  const logos = [js, react, redux, jest, enzyme, nodejs, scss, css, html, mocha, chai];
+
+  const displaySkills = logos.map(l => <img className="logo" src={l} />)
 
   return (
     <section className='Contact'>
@@ -12,8 +28,14 @@ export default function Contact() {
         {displayData}
       </section>
       <form className="download-form" method="get" action={resume}>
-        <button className="download-btn" type="submit">Download my resume!</button>
+        <button className="download-btn hvr-sweep-to-right" type="submit">Download my resume!</button>
       </form>
+      <section className="skills">
+        <h2>skills</h2>
+        <section className="skills-section">
+          {displaySkills}
+        </section>
+      </section>
     </section>
   )
 }
